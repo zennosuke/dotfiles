aa244c2a10f40133c1916c8f1e4d0d992f20ff12
Zenei Esaki (江﨑 全英) <z000907@cb.local>
2018-12-28 06:42:57 +0000

    first commit

---
+++ mklink.bat
@@ -0,0 +1,6 @@
+mklink C:\Program Files\Users\z000907\.gvimrc C:\Program Files\Users\z000907\dotfiles\.gvimrc
+mklink C:\Program Files\Users\z000907\.vimrc C:\Program Files\Users\z000907\dotfiles\.vimrc
+rem mklink C:\Program Files\vim80-kaoriya-win64\vimrc C:\Program Files\vim80-kaoriya-win64\dotfiles\vimrc
+mklink /D C:\Program Files\vim80-kaoriya-win64\vim80 C:\Program Files\vim80-kaoriya-win64\dotfiles\vim80
+exit 0
+
