d3ffa19d3dcec68e8fa3378bb2641cc08adce112
zennosuke <zen.e.0719@gmail.com>
2019-04-24 16:50:38 +0900

    Update dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# git cloneしたら、これを実行してシンボリックリンクを張る
+
 ln -sf ~/github/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/github/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/github/dotfiles/.bashrc ~/.bashrc
